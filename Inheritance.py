64ff2eba24e673d4cc2e000ac30652186b8e8470
Prutha Patel <119762242+pruthapatel3011@users.noreply.github.com>
2023-09-12 23:21:14 -0400

    Add files via upload

---
+++ Inheritance.py
@@ -0,0 +1,22 @@
+class Animal:
+
+    alive = True
+    def eat(self):
+        print("This animal is eating")
+    def sleep(self):
+        print("This animal is sleeping")
+
+class Rabbit(Animal):
+    pass
+class Fish(Animal):
+    pass
+class Hawk(Animal):
+    pass
+
+rabbit = Rabbit()
+fish=Fish()
+hawk=Hawk()
+
+print(rabbit.alive)
+fish.eat()
+hawk.sleep()
